7c8dcba8ef7a9101a2ae93d3e072d83cfa1d8454
rossm55 <rossm55@gmail.com>
2025-07-03 14:43:49 -0400

    refactored ClinicalNotesExtractor to read through the notes as a lazy function, making it much faster. The separate organizer class is also redundant now, and has been removed.

---
+++ calvin_utils/gpt_sys_review/txt_utils/ClinicalNotesExtractor.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import os
+
+class ClinicalNotesExtractor:
+    """
+    A class to process clinical notes from an RPDR request and organize them in a csv.
+
+    Attributes:
+    - input_file (str): The path to the text file to be preprocessed.
+    - output_dir (str): The directory where the output csv will be saved.
+
+    Methods:
+    - extract_headers: takes the notes file and gets the headers for each note
+    - extract_notes: takes the headers and uses them to get each separate note
+    """
+    def __init__(self, input_file, output_dir, separator="|", MRN_str='MRN', report_end_str='[report_end]'):
+        self.MRN_str = MRN_str
+        self.report_end_str=report_end_str
+        self.separator=separator
+        self.input_file=input_file
+        self.output_dir=output_dir
+        # self.selected_mrns=selected_mrns
+        self._prep_out_dir()
+    
+    ### Internal API ###
+    def _prep_out_dir(self):
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir, exist_ok=True)
+
+    def _file_reader(self):
+        """Generator to read the input file line by line."""
+        for row in open(self.input_file, "r"):
+            yield row
+
+    ### Public API ###
+
+    def split_by_subject(self):
+        """Splits the file so that each subject is in their own file."""
+
+        reader=self._file_reader()
+        
+        # gets the file header, so we know where the MRN column is in the note headers
+        for row in reader:
+            file_header=row
+            mrn_index=file_header.split(self.separator).index(self.MRN_str)
+            break
+        
+        note=file_header
+
+        for row in reader:
+            
+            note+=row
+
+            if self.separator in row:
+                header=row
+
+            if (self.report_end_str in row) or (row == ''):
+
+                if header=='':
+                    raise ValueError("Header is empty")
+                
+                mrn=header.split(self.separator)[mrn_index]
+
+                with open(os.path.join(self.output_dir, f'{mrn}.txt'), 'a') as subject_file:
+                    subject_file.write(note)
+
+                note=file_header
+                header=''
+    
+    def generate_master_list(self):
+        """Generates a master list of all subjects and their notes."""
+        
+        master_list = []
+        for filename in os.listdir(self.output_dir):
+            if filename.endswith('.txt'):
+                mrn = filename.split('.')[0]
+                filepath = os.path.join(self.output_dir, filename)
+                master_list.append({'MRN': mrn, 'filepath': filepath})
+        
+        self.master_list = pd.DataFrame(master_list)
+        
+    def filter_master_list(self, selected_mrns=None):
+        """Filters the master list based on selected MRNs."""
+        
+        if selected_mrns:
+            selected_mrns_int = [int(mrn) for mrn in selected_mrns]
+            self.master_list = self.master_list[self.master_list['MRN'].astype(int).isin(selected_mrns_int)]
+        else:
+            print("No list of MRNs given, keeping all subjects in the master list.")
+        
+        # return self.master_list
+
+    def save_master_list(self):
+        """Saves the master list to a CSV file."""
+
+        master_list_path = os.path.join(self.output_dir, 'master_list.csv')
+        self.master_list.to_csv(master_list_path, index=False)
+        print(f"Master list saved to {master_list_path}")
+
+
+    def run(self, selected_mrns=None):
+        self.split_by_subject()
+        self.generate_master_list()
+        self.filter_master_list(selected_mrns)
+        self.save_master_list()
+        return self.master_list
+    
+    
