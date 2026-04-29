5b0580bc219d8deefbccd8cb6af0dca7cb541989
rossm55 <rossm55@gmail.com>
2026-04-29 11:50:02 -0400

    Most recent version of analyze_clinical_notes_in_azure

--- analyze_clinical_notes_in_azure.py
+++ analyze_clinical_notes_in_azure.py
@@ -15,7 +15,7 @@ import pandas as pd
 select_mrns=pd.read_csv('F:/hbs_study_patient_notes/hbs_ross_aryan_gpt_n1205.csv')['MRN']
 # select_mrns=None
 
-output_dir='F:/Code/outputs/hbs_depression_memory_3/'
+output_dir='/Users/rm026/Documents/hbs_study_patient_notes/hbs_depression_memory_3/'
 
 # Provide the path to your OpenAI API key
 api_key_path = "F:/Code/openai-key.txt"
@@ -61,11 +61,6 @@ extraction_question_sets = ['hemiparesis','nih_stroke','depression','moca']
 # - "severity_with_explanations"
 extraction_answer_format="binary_with_unknown_and_explanations"
 
-evaluate_accuracy=False
-#Additional files for auc calculation
-ground_truth_path = "/Users/rm026/Documents/code/ReviewPyper_testing/redacted_msa_ground_truth_no_maybes.csv"
-iteration_history_path = "/Users/rm026/Documents/code/ReviewPyper_testing/all_patients_gpt4_bars_redacted_iteration_history.csv"
-accuracy_image = output_dir+"iteration_accuracy.png"
 
 # ################################################################################
 # # DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING! #
@@ -73,8 +68,8 @@ accuracy_image = output_dir+"iteration_accuracy.png"
 import os
 import json
 
-inclusion_questions_json = json.load(open('inclusion_questions.json', encoding='UTF-8'))
-inclusion_questions={q:name for set_name in inclusion_question_sets for q, name in inclusion_questions_json[set_name].items()}
+# inclusion_questions_json = json.load(open('inclusion_questions.json', encoding='UTF-8'))
+# inclusion_questions={q:name for set_name in inclusion_question_sets for q, name in inclusion_questions_json[set_name].items()}
     
 extraction_questions_json = json.load(open('extraction_questions.json', encoding='UTF-8'))
 extraction_questions={q:name for set_name in extraction_question_sets for q, name in extraction_questions_json[set_name].items()}
@@ -83,39 +78,38 @@ master_list_path = output_dir+"master_list.csv"
 master_list_excel_path = output_dir+"master_list.xlsx"
 json_file_path = output_dir+"json/_emr_labeled_sections.json"
 
-# from calvin_utils.gpt_sys_review.txt_utils import ClinicalNotesExtractor, TextPreprocessor
-# counter=0
-# while os.path.isfile(master_list_path): #rename if master list already exists
-#     counter+=1
-#     master_list_path='/'.join(master_list_path.split('/')[:-1]+[f'master_list_{counter}.csv'])
+from calvin_utils.gpt_sys_review.txt_utils import ClinicalNotesExtractor, TextPreprocessor
+counter=0
+while os.path.isfile(master_list_path): #rename if master list already exists
+    counter+=1
+    master_list_path=master_list_path.replace('.csv',f"_{counter}.csv")
 
+# extractor=ClinicalNotesExtractor(notes_file_list, mrn_file, output_dir, filter_list=select_mrns, debug=True)
+from calvin_utils.gpt_sys_review.txt_utils import PerNoteExtracting
+extractor=PerNoteExtracting(notes_file_list, mrn_file, output_dir)
 
-# extractor=ClinicalNotesExtractor(notes_file_list, mrn_file, output_dir, filter_list=select_mrns)
-# preprocessor = TextPreprocessor(input_dir=output_dir)
-# if counter==0:    
-#     note_df=extractor.run()
-#     preprocessed_path = preprocessor.process_files()
-# else: # if extraction has already been done, just generate a blank master list 
-#     extractor.generate_master_list()
-#     extractor.save_master_list()
-#     preprocessed_path = preprocessor.output_dir
+preprocessor = TextPreprocessor(input_dir=output_dir)
+if counter==0:    
+    note_df=extractor.run()
+    preprocessed_path = preprocessor.process_files()
+else: # if extraction has already been done, just generate a blank master list 
+    extractor.generate_master_list()
+    extractor.save_master_list()
+    preprocessed_path = preprocessor.output_dir
 
 article_type = 'emr'  # 'case', 'research', 'emr', or 'other'
 
-# from calvin_utils.gpt_sys_review.json_utils import SectionLabeler
-# ## Initialize the SectionLabeler class and process the files
-# ## TODO: update this to check that the labeled sections file has all the 
-# ## subjects in it, not just that it exists.
-# if os.path.exists(output_dir+"json/_emr_labeled_sections.json"):
-#     print(f"Found existing labeled sections at {output_dir+'json/_emr_labeled_sections.json'}. Skipping section labeling step.")
-# else:
-#     section_labeler = SectionLabeler(folder_path=preprocessed_path, 
-#                                     article_type="emr", 
-#                                     api_key_path=api_key_path,)
-#     section_labeler.process_files(label_files=segment_file)
+from calvin_utils.gpt_sys_review.json_utils import SectionLabeler
+## Initialize the SectionLabeler class and process the files
+## TODO: update this to check that the labeled sections file has all the 
+## subjects in it, not just that it exists.
+section_labeler = SectionLabeler(folder_path=preprocessed_path, 
+                                article_type="emr", 
+                                api_key_path=api_key_path,)
+section_labeler.process_files(label_files=segment_file)
 
 
-# Ask inclusion/exclusion questions
+# # Ask inclusion/exclusion questions
 # from calvin_utils.gpt_sys_review.gpt_utils.openai_json_evaluator import OpenAIJsonEvaluator
 # evaluator = OpenAIJsonEvaluator(api_key_path=api_key_path,
 #                                 json_file_path=json_file_path, 
@@ -144,32 +138,34 @@ article_type = 'emr'  # 'case', 'research', 'emr', or 'other'
 #                                               raw_results_path=exclusion_raw_path,)
 
 
-# extraction_debug=True
-# if extraction_debug and os.path.exists('/Users/rm026/Documents/code/ReviewPyper/debug_openai_chat.txt'):
-#     os.remove('/Users/rm026/Documents/code/ReviewPyper/debug_openai_chat.txt')
-# elif extraction_debug and os.path.exists('/Users/rm026/Documents/code/ReviewPyper/error_log.txt'):
-#     os.remove('/Users/rm026/Documents/code/ReviewPyper/error_log.txt')
+extraction_debug=True
+if extraction_debug and os.path.exists('/Users/rm026/Documents/code/ReviewPyper/debug_openai_chat.txt'):
+    os.remove('/Users/rm026/Documents/code/ReviewPyper/debug_openai_chat.txt')
+elif extraction_debug and os.path.exists('/Users/rm026/Documents/code/ReviewPyper/error_log.txt'):
+    os.remove('/Users/rm026/Documents/code/ReviewPyper/error_log.txt')
     
-# from calvin_utils.gpt_sys_review.gpt_utils.openai_json_evaluator import OpenAIJsonEvaluator
-# evaluator = OpenAIJsonEvaluator(api_key_path=api_key_path,
-#                                 json_file_path=json_file_path, 
-#                                 keys_to_consider=['emr'],
-#                                 question_type="emr_strict_extraction",
-#                                 question=extraction_questions,
-#                                 answer_format=extraction_answer_format,
-#                                 retain_chunks=True, 
-#                                 # include_explanations=True,
-#                                 test_mode=test_mode,
-#                                 model_choice=extraction_model_name,
-#                                 max_workers=50,
-#                                 debug=extraction_debug,
-#                                 is_azure=True,
-#                                 deployment_id=extraction_model_name,
-#                                 api_base=api_base,
-#                                 api_version=api_version)
-# answers = evaluator.evaluate_all_files()
-# extraction_chunks_dir=evaluator.chunk_dir
-# evaluated_json_path = evaluator.save_to_json(answers)
+from calvin_utils.gpt_sys_review.gpt_utils.openai_json_evaluator import OpenAIJsonEvaluator
+evaluator = OpenAIJsonEvaluator(api_key_path=api_key_path,
+                                json_file_path=json_file_path, 
+                                keys_to_consider=['emr'],
+                                question_type="emr_strict_extraction",
+                                question=extraction_questions,
+                                answer_format=extraction_answer_format,
+                                retain_chunks=True, 
+                                # include_explanations=True,
+                                test_mode=test_mode,
+                                model_choice=extraction_model_name,
+                                max_workers=50,
+                                debug=extraction_debug,
+                                is_azure=True,
+                                deployment_id=extraction_model_name,
+                                api_base=api_base,
+                                api_version=api_version)
+answers = evaluator.evaluate_all_files(chunk_by_date=True)
+if answers is False:
+    exit()
+extraction_chunks_dir=evaluator.chunk_dir
+evaluated_json_path = evaluator.save_to_json(answers)
 
 severity_dict = {
     0: ["unknown",'no info', 'no information', 'not mentioned', 'not present'],
@@ -181,12 +177,10 @@ from calvin_utils.gpt_sys_review.json_utils import CustomSummarizer
 custom_summarizer = CustomSummarizer(json_path=output_dir+"json_evaluated/emr_strict_extraction_evaluations.json",
                                     #  json_path=evaluated_json_path, 
                                      answer_format=extraction_answer_format,
-                                     summary_type='mapping', 
-                                     api_key_path=api_key_path,
+                                     summary_type='mapping',
                                     #  chunks_dir=extraction_chunks_dir, 
                                      is_azure=True, 
                                      debug=False,
-                                    #  severity_mapping=severity_dict if extraction_answers_binary else None
                                      severity_mapping=severity_dict,
                                      deployment_id=extraction_model_name, 
                                      api_base=api_base, api_version=api_version,
@@ -202,10 +196,9 @@ from calvin_utils.gpt_sys_review.gpt_utils.temporal_analysis import TemporalPlot
 plotter = TemporalPlotter(json_path=output_dir+"json_evaluated/emr_strict_extraction_evaluations.json", output_dir=output_dir+"plots/")
 onset_summary_path = plotter.run()
 
-if onset_summary_path:
-    from calvin_utils.gpt_sys_review.txt_utils import PostProcessing
-    PostProcessing.update_emr_master_list(master_list_path=master_list_path, 
-                                                  raw_results_path=onset_summary_path,)
+from calvin_utils.gpt_sys_review.txt_utils import PostProcessing
+PostProcessing.update_emr_master_list(master_list_path=master_list_path, 
+                                      raw_results_path=onset_summary_path,)
 
 PostProcessing.finalize_master_list(master_list_path=master_list_path,
                                           questions_dict=extraction_questions
@@ -215,18 +208,6 @@ PostProcessing.finalize_master_list(master_list_path=master_list_path,
 import pandas as pd
 excelDf = pd.read_csv(master_list_path)
 excelDf.to_excel(master_list_excel_path, index=False)
-
-if evaluate_accuracy:
-    from calvin_utils.evaluate_iterations import IterationEvaluator
-    ev = IterationEvaluator(
-        ground_path=ground_truth_path,
-        master_path=master_list_path,
-        history_path=iteration_history_path,
-        plot_path=accuracy_image,
-        reset=False,
-    )
-    row = ev.run()
-    print(row)
 
 print("Done!")
 
